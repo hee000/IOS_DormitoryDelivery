da657f30b2580f8c4f49af13fe73439ede235bd7
hee000 <79568286+hee000@users.noreply.github.com>
2021-11-15 15:04:21 +0900

    Initial Commit

---
+++ DormitoryDelivery/DormitoryDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormitoryDeliveryApp.swift
+//  DormitoryDelivery
+//
+//  Created by cch on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct DormitoryDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
